cacdd6ccd9d9c7050538250c721a2bc38199f39d
DuarteFrugoli <DuarteFrugoli@users.noreply.github.com>
2026-05-02 09:29:33 +0000

    deploy: e3c5921947f923f3a2d766b697227d42b62c6618

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1651474505" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1754673728" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
